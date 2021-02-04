fc63e7b240b403400049b46b3ffc82230ba0928d
Ibrahim <Ibrahim@DESKTOP-V7L6GKP>
2021-02-04 12:25:51 +0300

    First Commit

---
+++ First Practical Home work/src/First_Practical_Homwork.java
@@ -0,0 +1,12 @@
+//This program is written by Hatem
+//Date : 4/2/2021
+//Course Name : 212CSS-4
+public class First_Practical_Homwork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Hatem AL-Zmanan");
+ 
+	}
+
+}
